4542cb38217d0ca2ed12be8a1ea4e5f828371dc6
Jessica Woo <jlwoo@outlook.com>
2023-07-25 22:00:24 -0400

    update fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-# add your code here
+for num in range (101):
+    string = ""
 
+    if num % 3 == 0:
+        print ('Fizz')
+    if num % 5 == 0:
+        print ('Buzz')    
+    if num % 3 !=0 and num % 5 !=0:
+        string = string + str(num)
+    print (string)
